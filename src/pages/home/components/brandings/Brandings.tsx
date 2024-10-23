f50ee5455d31a1b583cc7d1ff934583d327bc8fc
DaniRicher <danielalvarado@dbscol.com>
2024-10-23 16:30:32 -0500

    añadiendo array de empresas

--- src/pages/home/components/brandings/Brandings.tsx
+++ src/pages/home/components/brandings/Brandings.tsx
@@ -6,7 +6,7 @@ export const Brandings = () => {
   return (
     <>
         <div className="section-with-background">
-            <h1>Sección con fondo claro</h1>
+            {/* <h1>Sección con fondo claro</h1> */}
             {/* Contenido de la sección */}
         </div>
     </>
---
+++ src/shared/brandings.ts
@@ -0,0 +1,5 @@
+import HeroMotoCorp from '../assets/images/brandings/Hero_MotoCorp.jpg';
+import RemoLab from '../assets/images/brandings/RemoLab.jpg';
+import VaxTrials from '../assets/images/brandings/VaxTrials.png';
+
+export { HeroMotoCorp, RemoLab, VaxTrials }
