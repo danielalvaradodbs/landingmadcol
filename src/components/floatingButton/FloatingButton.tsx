0f50cf516232e6f7552fa7f75dac8a8b223a6332
Daniel Alvarado <danielalvarado@madcritter.com>
2025-07-29 11:57:23 -0500

    edit numero

--- src/components/floatingButton/FloatingButton.tsx
+++ src/components/floatingButton/FloatingButton.tsx
@@ -12,7 +12,11 @@ export const FloatingButton = ({ isVisible }: any) => {
     // const message = encodeURIComponent('¡Hola! Quisiera más información.');
     // https://api.whatsapp.com/send?phone=3186954120&text=%C2%A1Hola%21+Quisiera+m%C3%A1s+informaci%C3%B3n&utm_source=whatssapp_rafa&utm_medium=cpc&utm_campaign=mad_critter_leads_whatsapp_rafa
     // const url = `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${message}`;
-    const url = `https://api.whatsapp.com/send?phone=3186954120&text=%C2%A1Hola%21+Quisiera+m%C3%A1s+informaci%C3%B3n&utm_source=whatssapp_rafa&utm_medium=cpc&utm_campaign=mad_critter_leads_whatsapp_rafa`;
+    // const url = `https://api.whatsapp.com/send?phone=3186954120&text=%C2%A1Hola%21+Quisiera+m%C3%A1s+informaci%C3%B3n&utm_source=whatssapp_rafa&utm_medium=cpc&utm_campaign=mad_critter_leads_whatsapp_rafa`;
+    const baseUrl = 'https://api.whatsapp.com/send';
+    const phone = '3186954120';
+    const message = '¡Hola Mad Critter!  Quiero contarles un poco sobre mi proyecto y escuchar sus ideas. ¿Me pueden guiar?';
+    const url = `${baseUrl}?phone=${phone}&text=${encodeURIComponent(message)}&utm_source=whatssapp_rafa&utm_medium=cpc&utm_campaign=mad_critter_leads_whatsapp_rafa`;
 
     window.open(url, '_blank');
   };
