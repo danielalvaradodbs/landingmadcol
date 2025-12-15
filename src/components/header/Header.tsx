a50f7a1e7a93fc4be979b0ce87ab24c32a63fd44
Daniel Alvarado <danielalvarado@madcritter.com>
2025-12-15 15:52:42 -0500

    ajustes 2

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -60,7 +60,7 @@ export const Header = () => {
           setIsCentered(false);
         }
       });
-    }, { threshold: [0.8] });
+    }, { threshold: [0.7] });
 
     const conceptSection = document.querySelector('.breakSchemes');
 
